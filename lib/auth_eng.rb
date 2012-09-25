17bd7405aa58798427f6fc1f30405abdf6eafcf2
Gustavo Lobo <gustavolobo90@gmail.com>
2012-09-25 11:17:12 +0100

    Trying to set up the engine in order to mount it.

--- lib/auth_eng.rb
+++ lib/auth_eng.rb
@@ -1,4 +1,12 @@
 require "auth_eng/engine"
 
 module AuthEng
+  class AuthEng < Rails::Engine
+    # Add a load path for this specific Engine
+    config.autoload_paths << File.expand_path("../lib/some/path", __FILE__)
+
+    initializer "auth_eng.add_middleware" do |app|
+      app.middleware.use AuthEng::Middleware
+    end
+  end
 end
