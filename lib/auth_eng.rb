96f3df36d13de71e62a744135096fbed5a9328cc
Gustavo Lobo <gustavolobo90@gmail.com>
2012-09-25 11:17:51 +0100

    Corrections made.

--- lib/auth_eng.rb
+++ lib/auth_eng.rb
@@ -3,7 +3,7 @@ require "auth_eng/engine"
 module AuthEng
   class AuthEng < Rails::Engine
     # Add a load path for this specific Engine
-    config.autoload_paths << File.expand_path("../lib/some/path", __FILE__)
+    config.autoload_paths << File.expand_path("../../", __FILE__)
 
     initializer "auth_eng.add_middleware" do |app|
       app.middleware.use AuthEng::Middleware
